781061102729bea821e421892e076f6649522a5c
ArbinIndia-Sudarshan <161278920+ArbinIndia-Sudarshan@users.noreply.github.com>
2025-03-18 17:22:33 +0530

    Initiate

---
+++ Invitey/Invitey/Models/Occasion.cs
@@ -0,0 +1,12 @@
+﻿namespace Invitey.Models
+{
+    public class Occasion
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Venue {  get; set; }
+        public DateTime DateTime { get; set; }
+        public int Days {  get; set; }
+        public string Description { get; set; }
+    }
+}
